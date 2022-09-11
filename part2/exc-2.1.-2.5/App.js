058c1bf374511edc910ab9167dc8eab7ea6d4e18
oxana-sh <60871090+oxana-sh@users.noreply.github.com>
2022-09-11 06:05:35 +0300

    Update App.js

--- part2/exc-2.1.-2.5/App.js
+++ part2/exc-2.1.-2.5/App.js
@@ -1,39 +1,52 @@
-import { useState } from 'react'
-
+import Course from './components/Course'
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
-  
-   
-  const [selected, setSelected] = useState(0)
-  const [vote, setVote] = useState([])
-
-  const clickVote = (props) => {
-    setVote (vote => [...vote, '{props.id} : 1'])
-    console.log();
+  const course =  [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
+  ]
 
-  const selectRandom= () => {
-    const selected = Math.floor(Math.random() * anecdotes.length);
-    setSelected(selected)
-    return (  
-           anecdotes[selected]  
-)
-}
-  return (
-    <>
-    <div>{anecdotes[selected]}</div>   
-    <button onClick={clickVote} id ='1' >Vote</button>
-    <button onClick={selectRandom}>Next Anecdote</button>
-    </>
-  )
+
+  return <Course course={course}  />
 }
 
 export default App
