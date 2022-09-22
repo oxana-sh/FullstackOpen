5d70df3dba6df2244f25374739111ae560337589
oxana-sh <60871090+oxana-sh@users.noreply.github.com>
2022-09-22 09:18:55 +0300

    Update Persons.js

--- part2/phonebook/components/Persons.js
+++ part2/phonebook/components/Persons.js
@@ -1,10 +1,11 @@
 import React from 'react';
-const Persons = ({persons,personToShow}) => {
+const Persons = ({personToShow, removePerson}) => {
         return ( 
         <div>
               <ul>
         {personToShow.map(person => 
-          <li key = {person.id}>{person.name} <b>{person.number}</b></li>
+          <li key = {person.id}>{person.name} <b>{person.number}</b> <button onClick={ ()=> removePerson(person.id)}>del</button></li>
+          
         )}
       </ul>
         </div>
